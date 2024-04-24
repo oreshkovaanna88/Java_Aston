da6e6d58d53454c3d040ba7d244ef926d3abcc99
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-04-24 22:23:18 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class Main
 {
 
     public static void main(String[] args)
-    { System.out.print("Введите номер задания: ");
+    { System.out.print(Номер задания: ");
         Scanner in = new Scanner(System.in);
         int task = in.nextInt();
         switch (task) {
