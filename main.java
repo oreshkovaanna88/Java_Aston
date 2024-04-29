ba1c741439635774558bd77b05d78a7a306b5681
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-04-29 22:13:33 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class Main
 {
 
     public static void main(String[] args)
-    { System.out.print(Номер задания: ");
+    { System.out.print("Введите номер задания: ");
         Scanner in = new Scanner(System.in);
         int task = in.nextInt();
         switch (task) {
@@ -16,13 +16,13 @@ public class Main
             case 4:
                 compareNumbers(); break;
             case 5:
-                getSumNumbers(); break;
+                System.out.println(getSumNumbers(9,10)); break;
             case 6:
                 giveTypeOfNumber(); break;
             case 7:
-                giveNumberType(); break;
+                System.out.println(giveNumberType(9)); break;
             case 8:
-                giveNumberOfLines(); break;
+                giveNumberOfLines("Welcome", 4); break;
             case 9:
                 getTypeOfYear(); break;
             case 10:
@@ -34,16 +34,16 @@ public class Main
             case 13:
                 fillDiagonalElements(); break;
             case 14:
-                fillArrayLength(); break;
+                fillArrayLength(15,5); break;
         }
+
     }
-// задание 1
+
     private static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    //  задание 2
     private static void checkSumSign() {
         int a = 0, b = 5, sum =0 ;
         sum = a+b;
@@ -51,7 +51,6 @@ public class Main
             System.out.println("Сумма положительная");}
         else {System.out.println("Сумма отрицательная");}
     }
-    //  задание 3
     private static void  printColor() {
         int value = 50;
         if (value <= 0) {
@@ -60,68 +59,44 @@ public class Main
             System.out.println("Желтый");}
         else {System.out.println("Зеленый");}
     }
-    //  задание 4
     private static void  compareNumbers() {
         int a = 50, b=20;
         if (a >= b) {
             System.out.println("a >= b");}
         else {System.out.println("a < b");}
     }
-    private static void  getSumNumbers() {
-        System.out.print("Ввести первое число: ");
-        Scanner in = new Scanner(System.in);
-        int first = in.nextInt();
-        System.out.print("Ввести второе число: ");
-        Scanner in1 = new Scanner(System.in);
-        int second = in1.nextInt();
+    private static boolean getSumNumbers(int first, int second) {
         int sum = first +second;
-        if (sum >= 10 && sum <=20) {
-            System.out.println("true");}
-        else {System.out.println("false");}
+        boolean value =(sum >= 10 && sum <=20);
+            return value;
     }
-    //  задание 5
     private static void  giveTypeOfNumber() {
-        System.out.print("Ввести число: ");
+        System.out.print("Введите число: ");
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
         if (number >= 0) {
             System.out.println("Число положительное");}
         else {System.out.println("Число отрицательное");}
     }
-    //  задание 6
-    private static void  giveNumberType() {
-        System.out.print("Ввести число: ");
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
-        if (number < 0) {
-            System.out.println("true");}
-        else {System.out.println("false");}
+    private static boolean  giveNumberType(int number) {
+       boolean value =(number<0);
+       return value;
     }
-    //  задание 7
-    private static void  giveNumberOfLines() {
-        System.out.print("Ввести строку: ");
-        Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-    //  задание 8
-        System.out.print("Ввести количество строк: ");
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+    private static void  giveNumberOfLines(String string, int number) {
         for (int i = 0; i < number; i++) {
             System.out.println(string);
         }
     }
-    //  задание 9
     private static void  getTypeOfYear() {
-        System.out.print("Введсти год: ");
+        System.out.print("Введите год: ");
         Scanner in = new Scanner(System.in);
         int numberofyear = in.nextInt();
         if (numberofyear < 0) {
-            System.out.println("Неправильное значение");}
+            System.out.println("Неверное значение");}
         if (( numberofyear %400==0)||(numberofyear > 0 && numberofyear %4==0 && numberofyear %100!=0)) {
             System.out.println("true");}
         else {System.out.println("false");}
     }
-    //  задание 10
     private static void  ChangeElements() {
         int[] numbers = { 1 , 0, 0, 0, 1 , 1 , 0, 0, 0};
         for (int i = 0; i < 9; i++) {
@@ -132,15 +107,13 @@ public class Main
             System.out.print(numbers[i] + " ");
         }
     }
-    //  задание 11
     private static void  FillElements() {
         int[] numbers = new int[100];
         for (int i = 0; i <100; i++) {
-            numbers[i] = i+1;
+           numbers[i] = i+1;
             System.out.print(numbers[i] + " ");
-        }
+            }
     }
-    //  задание 12
     private static void  MultiplyElements() {
         int[] numbers = { 5 ,50, 78, 10, 23 , 4 , 66, 3, 6};
         for (int i = 0; i < 9; i++) {
@@ -150,34 +123,25 @@ public class Main
             System.out.print(numbers[i] + " ");
         }
     }
-    //  задание 13
     private static void  fillDiagonalElements() {
-        System.out.print("Ввести размер массива: ");
+        System.out.print("Введите размер массива: ");
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
         int [][] array = new int [number][number];
         for (int i = 0; i < number; i++) {
+             array[i][i] = 1;
             array[i][number - i - 1] = 1;
         }
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < number; j++) {
-                array[i][i] = 1;
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
-    
-    //  задание 14
-    private static void  fillArrayLength() {
-        System.out.print("Длина массива: ");
-        int len, arr[];
-        Scanner in = new Scanner(System.in);
-        len = in.nextInt();
-        arr = new int[len];
-        System.out.print("Ввести значение массива: ");
-        Scanner scanner = new Scanner(System.in);
-        int initialValue = scanner.nextInt();
-        for(int i = 0; i < len; i++){
+    }
+    private static void  fillArrayLength(int len, int initialValue) {
+        int[] array = new int[len];
+        for(int i = 0; i < array.length; i++){
             System.out.print(initialValue + " ");
 
         }
