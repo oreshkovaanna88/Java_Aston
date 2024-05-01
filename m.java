45a4e77c57b950aad2510ab78a707f8e52046d98
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-05-01 23:01:07 +0300

    Update m.java

--- m.java
+++ m.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Employee[] employeearray = new Employee[5];
-        employeearray[0] = new Employee("Семенов Иван Петрович", "Менеджер по продажам", "semenov.ivan@mail.ru", "+375448785785","2000 руб.", "35 лет");
-        employeearray[1] = new Employee("Полежайкин Иосиф Ильич", "Главный бухгалтер", "polezh@mail.ru", "+375292334345","4500 руб.", "50 лет");
-        employeearray[2] = new Employee("Ландыш Далья Григорьевна", "Начальник отдела продаж", "darya9190@gmail.com", "+375331290789","3800 руб.", "48 лет");
-        employeearray[3] = new Employee("Автухович Кристина Петровна", "Юристконсульт", "avtush.kristina0102@gmail.com", "+375448879897","2000 руб.", "40 лет");
-        employeearray[4] = new Employee("Маськова Ирина Ивановна", "Заместитель директора", "irina.ivan@mail.ru", "+375253434344","4275 руб.", "42 года");
+        employeearray[0] = new Employee("Петров Иван Сергеевич", "Директор", "ivandir.petrov@mail.ru", "+375293939317","9000 руб.", "49 лет");
+        employeearray[1] = new Employee("Иванова Раиса Алексеевна", "Главный бухгалтер", "rusia73@mail.ru", "+3752253535355","5500 руб.", "51 лет");
+        employeearray[2] = new Employee("Сидоров Сергей Егорович", "Начальник отдела кадров", "sidorov1976@gmail.com", "+375337894512","4800 руб.", "48 лет");
+        employeearray[3] = new Employee("Цветкова Анна Сергеевна", "Секретарь", "anutik04@gmail.com", "+375291234556","2500 руб.", "20 лет");
+        employeearray[4] = new Employee("Зыль Марина Николаевна", "Уборщица", "marina.zyl@mail.ru", "+375254478989","1200 руб.", "62 года");
         for (int i = 0; i < 5; i++) {
             System.out.println(employeearray[i].getFIO());
             System.out.println(employeearray[i].getPosition());
