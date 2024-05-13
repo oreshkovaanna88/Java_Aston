e144e9606d0489a4a95bd41e535ee36335a4968d
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-05-13 19:39:59 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Main
 {
-
     public static void main(String[] args)
     { System.out.print("Введите номер задания: ");
         Scanner in = new Scanner(System.in);
