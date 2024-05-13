1b018137e5b918309ef0e24e9da0de3a2f84a107
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-05-13 22:44:06 +0300

    Update animal.java

--- урок 7/animal/animal.java
+++ урок 7/animal/animal.java
@@ -1,1 +1,35 @@
+package Hw7;
 
+public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void run(int distance) {
+    }
+
+    public void swim(int distance) {
+    }
+
+    //Находим количество отдельных животных
+    public static int getCountCertainAnimals(Animal[] animals) {
+        int countAnimals = 0;
+        for (Animal animal : animals) {
+            if (animal != null) {
+                countAnimals++;
+            }
+        }
+        return countAnimals;
+    }
+
+    //Находим общее количество животных
+    public static int getCountAllAnimals(int... countAnimal) {
+        int countAllAnimals = 0;
+        for (int i : countAnimal) {
+            countAllAnimals += i;
+        }
+        return countAllAnimals;
+    }
+}
