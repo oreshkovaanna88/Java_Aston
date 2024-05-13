8ab65f53c0c3056b05cd30db37ea50bc7eb35db1
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-05-13 22:22:05 +0300

    Update n.java

--- geometry/computation/n.java
+++ geometry/computation/n.java
@@ -1,1 +1,13 @@
+package hw7.geometry;
 
+public interface ComputationPerimeterAndArea {
+    
+    default double compulationPerimeter() {
+        return 0.0;
+    }
+
+    
+    default double compulationArea() {
+        return 0.0;
+    }
+}
