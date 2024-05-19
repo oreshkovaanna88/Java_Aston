09caea7ac9a48a00b258164be18e67775b4a39ca
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-05-19 22:50:15 +0300

    Create Animal.java

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,27 @@
+package animals;
+
+    class Animal
+    {
+        String type_of_animal;
+        static int count = 0;
+
+        public Animal(String type_of_animal)
+        {
+            this.type_of_animal = type_of_animal;
+
+        }
+
+        public static int getCount()
+        {
+            return count;
+        }
+        public Animal()
+        {
+            count++;
+        }
+
+        public void animal_run() {
+        }
+        public void animal_swim() {
+        }
+    }
