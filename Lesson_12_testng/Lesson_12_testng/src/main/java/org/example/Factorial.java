b02e87242a504c865921f1bd39a728c28afa43b5
oreshkovaanna88 <114339050+oreshkovaanna88@users.noreply.github.com>
2024-06-13 22:44:17 +0300

    Add files via upload

---
+++ Lesson_12_testng/Lesson_12_testng/src/main/java/org/example/Factorial.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Factorial {
+    public static int calculateFactorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
